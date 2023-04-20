5488c221649ae0c429661e623795afd9205c8421
ZJSmithMTU <131378531+ZJSmithMTU@users.noreply.github.com>
2023-04-20 10:33:04 -0400

    Update utils.py
    Added Mic Diameter Function.

--- acoustics/utils.py
+++ acoustics/utils.py
@@ -47,3 +47,27 @@ def _is_1d(given):
         return given[0]
     else:
         return given
+    
+    
+    import math
+
+def calculate_max_mic_diameter(freq, c=None):
+    """
+    Calculate the maximum diameter of a microphone in inches before the size of the microphone effects the sound field/acoustic wave, based on the given frequency and temperature.
+
+    Args:
+        freq (float): The frequency in Hz.
+        c (float, optional): The speec of sound in meters per second (m/s). If not provided, a default value of 343m/s is used.
+
+    Returns:
+        float: The maximum diameter of the microphone in inches before the size of the microphone effects the sound field/acoustic wave.
+    """
+    if c is None:
+        c = 343  # Default speed of of sound in m/s
+
+    # Calculate microphone min diameter in inches
+    w = 2 * math.pi * freq  # rad/s
+    d = 0.1/(w/c) # Make sure helmholts number is much less than one (i.e. kd<<1 ... (w/c)*d=0.1 solve for d)
+    maxDiameter = d * 39.37  # inch
+
+    return maxDiameter
